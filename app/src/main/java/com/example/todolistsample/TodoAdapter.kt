931a8e7f57755d498a1ab97cc3dd18adf54b3b6e
sena15-10 <threetensena15@outlook.jp>
2025-10-26 16:10:50 +0900

    今から画面部品の配置が終わったので、今からそれらをデザインしていく、ものを作成していきたいと思います。

---
+++ app/src/main/java/com/example/todolistsample/TodoAdapter.kt
@@ -0,0 +1,15 @@
+// TodoAdapter.kt
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolistsample.TodoItem
+
+// ① TodoAdapterクラス本体
+class TodoAdapter(private val todoList: List<TodoItem>)  {
+
+
+}
